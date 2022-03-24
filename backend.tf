ab0564698ba3ae52cb19538c9b974b3b0acfbd55
Reginald Jones <regjones16@outlook.com>
2022-03-24 14:01:23 -0400

    VPC exercise update

--- backend.tf
+++ backend.tf
@@ -4,10 +4,3 @@ terraform {
     key    = "sprint1/week2/training-terraform/terraform.tfstates"
   }
 }
-terraform {
-  backend "s3" {
-    bucket = "reggie-talent-academy-686520628199-tfstates"
-    key    = "sprint1/week2/training-terraform/terraform.tfstates"
-    dynamodb_table = "terraform-lock"
-  }
-}
