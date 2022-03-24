81fed7103f6334a7923ea309c228d973c19963a1
Reginald Jones <regjones16@outlook.com>
2022-03-24 09:36:22 -0400

    Setting up backend to work with terraform

---
+++ backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "reggie-talent-academy-686520628199-tfstates"
+    key    = "sprint1/week2/training-terraform/terraform.tfstates"
+  }
+}
+terraform {
+  backend "s3" {
+    bucket = "reggie-talent-academy-686520628199-tfstates"
+    key    = "sprint1/week2/training-terraform/terraform.tfstates"
+    dynamodb_table = "terraform-lock"
+  }
+}
---
+++ main.tf
@@ -0,0 +1,36 @@
+resource "aws_s3_bucket" "rh_project_bucket" {
+    bucket = "reggie-talent-academy-686520628199-tfstates"
+
+    lifecycle {
+      prevent_destroy = true
+    }
+
+    tags = {
+        Name = "talent-academy-686520628199-tfstates"
+        Environment = "Test"
+    }
+}
+
+resource "aws_s3_bucket_versioning" "version_my_bucket" {
+  bucket = aws_s3_bucket.rh_project_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock_tbl" {
+  name           = "terraform-lock"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags           = {
+    Name = "terraform-lock"
+  }
+}
